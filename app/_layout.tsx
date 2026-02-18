fc18d78c081d924bbd67dabe4b95cd54cb87726e
Sebastian Castillo <sebastiancastillo1402@gmail.com>
2026-02-17 19:41:58 -0500

    feat: Add custom fonts, create a themed header with an icon, and introduce a color constants file.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,59 @@
+import { EBGaramond_400Regular, EBGaramond_700Bold, useFonts } from '@expo-google-fonts/eb-garamond';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    EBGaramond_400Regular,
+    EBGaramond_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerTitleAlign: 'center',
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: () => (
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <MaterialCommunityIcons name="credit-card-multiple-outline" style={styles.icon} />
+              <Text style={styles.title}>BANCO</Text>
+            </View>
+          ),
+        }}
+      />
+    </Stack>
+  );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    letterSpacing: 0.1,
+    fontFamily: 'EBGaramond_700Bold',
+    color: '#40537e',
+  },
+  icon: {
+    marginRight: 10,
+    color: '#40537e',
+    fontSize: 24,
+  },
+})
---
+++ constants/theme.tsx
@@ -0,0 +1,7 @@
+export const Colors = {
+    primaryText: '#000',
+    background: '#fff',
+    secondaryText: '#6c757d',
+    success: '#28a745',
+    error: '#dc3545',
+}
